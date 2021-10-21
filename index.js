b95cb0b172d918b297c1442b055d14f86e8c7f54
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-10-21 10:10:54 -0400

    completed employee files, exported to index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // link to HTML page
 const generateHTML = require('./src/generateHTML');
 
-// different team members
+// import different team members
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
